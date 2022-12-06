7beded1a656e1b6ffbd804a86e1efce1595f7692
UNC GitHub <jrohan@ad.unc.edu>
2022-12-06 16:25:27 -0500

    updated rohan.js to be added to index.js

--- rohan.js
+++ rohan.js
@@ -1,21 +1,16 @@
-// Require minimist module (make sure you install this one via npm).
-// Require minimist module
 const args = require('minimist')(process.argv.slice(2))
-// See what is stored in the object produced by minimist
-//console.log('Command line arguments: ', args)
 
-// Store help text 
 const help = (`
 server.js [options]
---port, -p	Set the port number for the server to listen on. Port number must be an integer
+--port, -p  Set the port number for the server to listen on. Must be an integer
             between 1 and 65535.
 --debug, -d If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
---log		If set to false, no log files are written. Defaults to true where 
-            logs are always written to database.
---help, -h	Return this message and exit.
+--log       If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help, -h  Return this message and exit.
 `)
 // If --help, echo help text and exit
 if (args.help || args.h) {
@@ -23,22 +18,18 @@ if (args.help || args.h) {
     process.exit(0)
 }
 
-// Define app using express
+// creating app using express
 var express = require('express')
 var app = express()
 
-// Allow json body messages
-app.use(express.json());
-
-// Require fs
 const fs = require('fs')
-
-// Require morgan
 const morgan = require('morgan')
+const logdb = require('./services/database-accesslog.js')
+const userdb = require('./services/database-users.js')
 
-// Require database SCRIPT file
-const logdb = require('./src/services/database.js')
-
+// json body messages and express body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Server port
 const port = args.port || args.p || process.env.PORT || 5000
 // If --log=false then do not create a log file
@@ -47,15 +38,36 @@ if (args.log == 'false') {
 } else {
 // Use morgan for logging to files
     const logdir = './log/';
-
+    // if no logdir then create
     if (!fs.existsSync(logdir)){
         fs.mkdirSync(logdir);
     }
 // Create a write stream to append to an access.log file
-    const accessLog = fs.createWriteStream( logdir+'access.log', { flags: 'a' })
+    const accessLog = fs.createWriteStream(logdir+'access.log', { flags: 'a' })
 // Set up the access logging middleware
     app.use(morgan('combined', { stream: accessLog }))
 }
+
+const server = app.listen(port, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%", port))
+});
+
+if (args.debug || args.d) {
+    app.get('/app/log/access/', (req, res, next) => {
+        const stmt = logdb.prepare("SELECT * FROM accesslog").all();
+        res.status(200).json(stmt);
+    })
+
+    app.get('/app/error/', (req, res, next) => {
+        throw new Error('Error test works.')
+    })
+}
+
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+    res.status(200);
+});
 
 // Always log to database
 app.use((req, res, next) => {
@@ -76,6 +88,71 @@ app.use((req, res, next) => {
     //console.log(info)
     next();
 })
+
+app.post("/app/new/user", (req, res, next) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password,
+        bal: req.body.bal
+    }
+    const stmt = userdb.prepare('INSERT INTO userinfo (username, password, balance) VALUES (?, ?, ?)')
+    const info = stmt.run(data.user, data.pass, data.bal)
+    res.status(200).json(info)
+});
+
+// READ a list of users (HTTP method GET) at endpoint /app/users/
+app.get("/app/users", (req, res) => {   
+    try {
+        const stmt = userdb.prepare('SELECT * FROM userinfo').all()
+        res.status(200).json(stmt)
+    } catch {
+        console.error(e)
+    }
+});
+
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+    try {
+        const stmt = userdb.prepare('SELECT * FROM userinfo WHERE id = ?').get(req.params.id);
+        res.status(200).json(stmt)
+    } catch (e) {
+        console.error(e)
+    }
+
+});
+
+// UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
+    }
+    const stmt = userdb.prepare('UPDATE userinfo SET username = COALESCE(?,username), password = COALESCE(?,password) WHERE id = ?')
+    const info = stmt.run(data.user, data.pass, req.params.id)
+    res.status(200).json(info)
+});
+
+// UPDATE a single user balance (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+    let data = {
+        bal: req.body.bal
+    }
+    const stmt = userdb.prepare('UPDATE userinfo SET balance = COALESCE(?,bal) WHERE id = ?')
+    const info = stmt.run(data.bal, req.params.id)
+    res.status(200).json(info)
+});
+
+// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+    const stmt = userdb.prepare('DELETE FROM userinfo WHERE id = ?')
+    const info = stmt.run(req.params.id)
+    res.status(200).json(info)
+});
+// Default response for any other request
+app.use(function(req, res){
+    res.json({"message":"Endpoint not found. (404)"});
+    res.status(404);
+});
 
 // Spin the wheel once with a user bet
 function wheelSpin(bet, user_balance) {
@@ -110,70 +187,9 @@ function wheelSpinMult(bet, spins, user_balance) {
     return newBet
 }
 
-// Serve static HTML public directory
-app.use(express.static('./public'))
-
-// READ (HTTP method GET) at root endpoint /app/
-app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
-	res.status(200);
-});
-
 // Endpoint /app/spin/ that returns JSON {"spin":[credits won]} 
 // corresponding to the results of the random coin flip.
 app.get('/app/spin/', (req, res) => {
     const win = wheelSpin()
     res.status(200).json({ "spin" : win })
-});
-
-app.post('/app/flip/coins/', (req, res, next) => {
-    const flips = coinFlips(req.body.number)
-    const count = countFlips(flips)
-    res.status(200).json({"raw":flips,"summary":count})
-})
-
-app.get('/app/flips/:number', (req, res, next) => {
-    const flips = coinFlips(req.params.number)
-    const count = countFlips(flips)
-    res.status(200).json({"raw":flips,"summary":count})
-});
-
-app.post('/app/flip/call/', (req, res, next) => {
-    const game = flipACoin(req.body.guess)
-    res.status(200).json(game)
-})
-
-app.get('/app/flip/call/:guess(heads|tails)/', (req, res, next) => {
-    const game = flipACoin(req.params.guess)
-    res.status(200).json(game)
-})
-
-if (args.debug || args.d) {
-    app.get('/app/log/access/', (req, res, next) => {
-        const stmt = logdb.prepare("SELECT * FROM accesslog").all();
-	    res.status(200).json(stmt);
-    })
-
-    app.get('/app/error/', (req, res, next) => {
-        throw new Error('Error test works.')
-    })
-}
-
-// Default API endpoint that returns 404 Not found for any endpoints that are not defined.
-app.use(function(req, res){
-    const statusCode = 404
-    const statusMessage = 'NOT FOUND'
-    res.status(statusCode).end(statusCode+ ' ' +statusMessage)
-});
-
-// Start server
-const server = app.listen(port, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",port))
-});
-
-// Tell STDOUT that the server is stopped
-process.on('SIGINT', () => {
-    server.close(() => {
-		console.log('\nApp stopped.');
-	});
 });
